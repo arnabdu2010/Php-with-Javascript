84abe39191c58ad101777cc4f4939bb378b01883
arnabdu2010 <55645593+arnabdu2010@users.noreply.github.com>
2019-12-11 14:17:07 +0600

    Add files via upload

--- display.php
+++ display.php
@@ -1,36 +1,52 @@
+<style>
+td{
+	padding:10px;
+}
+</style>
+
+
+
+
 <?php
-   include("connection.php");
+   
+   include('connection.php');
    error_reporting(0);
+   
    $query ="SELECT * FROM STUDENT";
    $data =mysqli_query($conn,$query);
    $total =mysqli_num_rows($data);
    
-   if($total !=0){
+   if($total!=0){
 	   
 	   ?>
-	   
 	   <table>
 	    <tr>
-	       <th> Rollno </th>
-	       <th> Name </th>
+	       <th> rollno </th>
+	       <th> name </th>
 	       <th> class </th>
-		   </tr>
-		   <?php
-		   while($result =mysqli_fetch_assoc($data)){
-			   
-			   echo "<tr>
-			      <td>".$result['rollno']."</td>
-			       <td>".$result['name']."</td>
-			        <td>".$result['class']."</td>
-					</tr>";
-		   }
+		   <th colspan="2"> Operations </th>
+         </tr>
+		 
+		 <?php
+		    
+			while($result =mysqli_fetch_assoc($data)){
+				
+				echo "<tr>
+						<td>".$result['rollno']."</td>	
+						<td>".$result['name']."</td>	
+						<td>".$result['class']."</td>
+						<td> <a href='update.php?rn=$result[rollno]&na=$result[name]&cl=$result[class]'> Edit </a></td>
+						<td> Delete </td>
+                      </tr>";
+			}
    }
+       else{
+		   echo "no result found";
+	   }
+	   ?>
+	   </table>
 		   
-		   else{
-			   echo "no records found";
-   }
    
-   ?>
-   </table>
+  
    
     
